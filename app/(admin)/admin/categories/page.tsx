6c49cb64d503327398cb59657647dc9c16403103
Mehrdad khandan <149022241+Mehrdadkhandan1@users.noreply.github.com>
2026-01-04 04:30:58 +0330

    create category

---
+++ app/(admin)/admin/categories/page.tsx
@@ -0,0 +1,12 @@
+import CategoryForm from '@/features/Categories/components/CategoryForm'
+import React from 'react'
+
+const AddCategoryPage = () => {
+  return (
+    <div>
+        <CategoryForm />
+    </div>
+  )
+}
+
+export default AddCategoryPage
---
+++ features/Categories/components/CategoryForm.tsx
@@ -0,0 +1,74 @@
+"use client";
+import CustomButton from "@/components/cusotm-ui/CustomButton";
+import { Input } from "@/components/ui/input";
+import Image from "next/image";
+import { useState } from "react";
+
+const CategoryForm = () => {
+  const [preview, setPreview] = useState<string | null>(null);
+  const [file, setFile] = useState<File | null>(null);
+  const setIcon = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files && e.target.files.length > 0) {
+      const selectedFile = e.target.files[0];
+      setFile(selectedFile);
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setPreview(reader.result as string);
+      };
+      reader.readAsDataURL(selectedFile);
+    }
+  };
+  return (
+    <form className="flex flex-col gap-3">
+      <div>
+        <h3 className="mt-4 font-bold border-b-2 pb-2 border-primary inline-block ">
+          دسته بندی جدید
+        </h3>
+      </div>
+      <div className="flex items-center justify-center ">
+        <label
+          htmlFor="icon"
+          className=" border relative rounded-full flex items-center cursor-pointer justify-center p-2 border-primary size-20 "
+        >
+          <div className="absolute -left-2 bottom-0 bg-white rounded-full size-7 border  p-1 flex items-center justify-center">
+            <Image
+              src={"/Icons/trash.svg"}
+              alt="trash"
+              width={30}
+              height={30}
+            />
+          </div>
+          <Image
+            src={preview ? preview : "/Icons/camera.svg"}
+            alt="icon"
+            height={50}
+            width={50}
+            className={` w-full  h-full object-cover ${
+              !preview && "opacity-50 w-10! h-10!"
+            } `}
+          />
+        </label>
+        <Input
+          onChange={setIcon}
+          type="file"
+          name="icon"
+          id="icon"
+          className="fixed -left-full"
+        />
+      </div>
+      <div className="flex flex-col gap-3">
+        <label htmlFor="categoryName">نام دسته بندی</label>
+        <Input type="text" name="categoryName" />
+      </div>
+      <div className="flex flex-col gap-3">
+        <label htmlFor="slug">اسلاگ</label>
+        <Input type="text" name="slug" />
+      </div>
+      <CustomButton type="submit" >
+        ثبت دسته بندی
+      </CustomButton>
+    </form>
+  );
+};
+
+export default CategoryForm;
